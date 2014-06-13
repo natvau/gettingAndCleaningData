017abe120907802e367b127ee6ce6f952d96fc74
natvau <natvau@gmail.com>
2014-06-14 08:04:19 +1000

    minor changes

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# download data
+Getting the tidy data
+========================================================
 
-#source file
+# Download the data
+```{r}
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
+download.file(url=url, destfile='data', method='curl')
+unzip(zipfile="data")
+```
+
+# Source the file
+```{r}
 source('run_analysis.r')
+```
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 # 1. Merges the training and the test sets to create one data set.
-setwd('~/Desktop/coursera/data specialization/3.GettingAndCleaningData/project/UCI HAR Dataset/')
+setwd(paste(getwd(), "/UCI HAR Dataset/", sep=""))
 train <- read.table("train/X_train.txt", header=FALSE, sep = "")
 train <- cbind(train, read.table("train/subject_train.txt"), read.table("train/y_train.txt"))
 test <- read.table("test/X_test.txt", header=FALSE, sep = "")
@@ -7,20 +7,20 @@ test <- cbind(test, read.table("test/subject_test.txt"), read.table("test/y_test
 data <- rbind(train, test)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-features <- read.table('features.txt', header=FALSE, stringsAsFactors=FALSE)
-features <- make.names(features[,'V2'])
-mean_std <- data[,grep(pattern='std|mean', x=features, ignore.case=TRUE)]
+features <- read.table("features.txt", header=FALSE, stringsAsFactors=FALSE)
+features <- make.names(features[,"V2"])
+mean_std <- data[,grep(pattern="std|mean", x=features, ignore.case=TRUE)]
 
 # 3. Uses descriptive activity names to name the activities in the data set
-activity_labels <- read.table('activity_labels.txt', header=FALSE, stringsAsFactors=FALSE)
-activity_labels <- apply(activity_labels, 1, function(x) unlist(strsplit(x, split=' ')))
+activity_labels <- read.table("activity_labels.txt", header=FALSE, stringsAsFactors=FALSE)
+activity_labels <- apply(activity_labels, 1, function(x) unlist(strsplit(x, split=" ")))
 data[,563] <- factor(as.factor(data[,563]), labels=activity_labels[2,])
 
 # 4. Appropriately labels the data set with descriptive activity names. 
-features <- read.table('features.txt', header=FALSE, stringsAsFactors=FALSE)
-features <- make.names(features[,'V2'])
-features[562] = 'subject'
-features[563] = 'activity'
+features <- read.table("features.txt", header=FALSE, stringsAsFactors=FALSE)
+features <- make.names(features[,"V2"])
+features[562] = "subject"
+features[563] = "activity"
 colnames(data) <- features
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
